6765039475b71323599c97a8019a7ee677fe414f
sami8890 <samigabol12@gmail.com>
2025-03-29 12:56:07 +0500

    e

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import LandingPage from './components/landing-page'
-import FullLandingPage from './components/full-landing-page'
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <LandingPage/>
-      {/* <FullLandingPage/> */}
-    </div>
+    </main>
   )
 }
